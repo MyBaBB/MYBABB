40cc8c3894bcf682170e3b82b87573db343383b1
Brett <ZappnSlapp@gmail.com>
2023-07-12 08:29:24 -0700

    I added custom colors of the woodduck image   type bg-var  for results

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-
+# DaveGrayTuts
+TailwindTut's
+This is where Dave Gray's lesson 02 will be hosted and probably kept as the new junk repository because I am getting cluttered with empty repositories.
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,42 @@ module.exports = {
   content: ["./build/*.html", "./build/js/*.js"],
   theme: {
     extend: {
+     colors: { 
+       
+           /* custom colors from the WoodDuck image */
+
+           varDARKBEAKTOP:        "#785001",  
+           varMEDBEAKTOP:         "#b67902", 
+           varLIGHTBEAKTOP:       "#fca802", 
+         
+           varDARKDUCKHEAD:       "#1e3b28",       
+           varMEDDUCKHEAD:        "#284f35",    
+           varLIGHTDUCKHEAD:      "#528f85", 
+           
+           varDARKBLUEFEATHER:    "#24558d",  
+           varMEDBLUEFEATHER:     "#2a61a1",  
+           varLIGHTBLUEFEATHER:   "#419aff", 
+           
+           varDARKDUCKCHEEKS:     "#362e47", 
+           varMEDDUCKCHEEKS:      "#6b5b8d", 
+           varLIGHTDUCKCHEEKS:    "#a088d2",
+           
+           varDARKNECK:           "#412a32",  
+           varMEDNECK:            "#98648d", 
+           varLIGHTNECK:          "#c68199", 
+          
+           varDARKWING:           "#3c302e", 
+           varMEDWING:            "#806661", 
+           varLIGHTWING:          "#cfa69d", 
+         
+           varDARKBEAK:           "#70181a", 
+           varMEDBEAK:            "#962023",
+           varLIGHTBEAK:          "#d72d32", 
+         
+           varDARKSPACE:          "#08121c", 
+           varMEDSPACE:           "#1b3854", 
+           varLIGHTSPACE:         "#264d73", 
+
       screens: {
          'widescreen': { 'raw': '(min-aspect-ratio: 3/2)'},
          'tallscreen': { 'raw': '(max-aspect-ratio: 13/20)'},
@@ -25,4 +61,4 @@ module.exports = {
   plugins: [],
 }
 
- 
+}
