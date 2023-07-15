2fedbd8f8de8ffc31916eda9d46395c629b878c8
Brett <ZappnSlapp@gmail.com>
2023-07-15 12:53:04 -0700

    retry

--- build/js/main.js
+++ build/js/main.js
@@ -12,6 +12,17 @@ const initApp = () => {
 }
 document.addEventListener('DOMContentLoaded', initApp)
 
+ 
+          const images = document.querySelectorAll('img');
+          let index = 0;
 
+          function changeImage() {
+            images[index].style.opacity = 0;
+            index = (index + 1) % images.length;
+            images[index].style.opacity = 1;
+          }
+
+          setInterval(changeImage, 10000);
+        
 
  
