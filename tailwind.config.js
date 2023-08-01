166c5fbdb430882e9fa5ba573d9dbbda70def2f3
Brett <ZappnSlapp@gmail.com>
2023-08-01 07:04:38 -0700

    I put in my crazy take the pill image

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,7 @@
       'medievalsharp': ['MedievalSharp', 'cursive'],
       'handjet': ['Handjet', 'cursive'],
       'abrilfatface': ['Abril Fatface', 'cursive'],
+      'Vast Shadow': ['Vast Shadow', 'cursive'],
     },
 
 
