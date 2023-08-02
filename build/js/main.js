0734cce352db5ce5c34b2a14c171092709250da9
Brett <ZappnSlapp@gmail.com>
2023-08-02 14:09:04 -0700

    I modified a cool old adobe  church and added a neon sign to it with flashing crosses on the roof.

--- build/js/main.js
+++ build/js/main.js
@@ -58,6 +58,30 @@ function toggleImagesAlt() {
 
 // Call the toggleImages function every 5 seconds
 setInterval(toggleImagesAlt, 2000);
+// Get the image elements by their IDs
+/* xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx */
+
+
+const imageAlt3 = document.getElementById('imageAlt3');
+const imageAlt4 = document.getElementById('imageAlt4');
+
+// Set the initial state of the images
+imageAlt3.style.display = 'block';
+imageAlt4.style.display = 'none';
+
+// Create a function to toggle the images
+function toggleImagesAlt2() {
+  if (imageAlt3.style.display === 'block') {
+    imageAlt3.style.display = 'none';
+    imageAlt4.style.display = 'block';
+  } else {
+    imageAlt3.style.display = 'block';
+    imageAlt4.style.display = 'none';
+  }
+}
+
+// Call the toggleImages function every 5 seconds
+setInterval(toggleImagesAlt2, 2000);
 
         
 
