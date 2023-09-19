bb334db7087f902a06eb51152f4820292fbe50f2
Brett <ZappnSlapp@gmail.com>
2023-09-18 19:36:15 -0700

    underline submit

---
+++ build/js/app.js
@@ -0,0 +1,10 @@
+
+const searchButton = document.querySelector('#search-button')
+const searchButtonContent = document.querySelector('#search-button div')
+
+searchButton.addEventListener('click', toggleButton) 
+
+function toggleButton() {
+  searchButtonContent.classList.toggle('loading')
+  
+}
